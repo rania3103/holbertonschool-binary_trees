cbfb329452047c1147b4ba33c5e092882e861039
Mariam Charrada <charradamariam@yahoo.com>
2023-09-17 02:06:28 -0700

    free newnode

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL || newnode == NULL)
+	{
+		free(newnode);
 		return (NULL);
+	}
 
 	newnode->n = value;
 
