1ee48e97e0033b9a0f61059ab34853e66578a8c9
rania3103 <raniaabassi33@gmail.com>
2023-09-17 16:17:42 +0100

    fix code

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,17 +6,15 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL)
+	binary_tree_t *grandpa;
+
+	if (node == NULL || node->parent == NULL || grandpa == NULL)
 		return (NULL);
 
-	binary_tree_t *grandpa = (node->parent)->parent;
-
-	if (node->parent == NULL || grandpa == NULL)
-		return (NULL);
+	grandpa = (node->parent)->parent;
 
 	if (grandpa->right == node->parent)
 		return (grandpa->left);
 	else
 		return (grandpa->right);
-	return (NULL);
 }
