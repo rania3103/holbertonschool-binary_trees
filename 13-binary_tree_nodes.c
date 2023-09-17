cff70e87bdbb78f833b44022e4a44ff3626263f0
amaalyy <amal.elzowawi@gmail.com>
2023-09-17 20:32:34 +0100

    fix error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,6 +16,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	count = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1;
+	count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
 	return (count);
 }
