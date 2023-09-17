34ce343db5d01a1cc1b893a06193a7eea378b206
rania3103 <raniaabassi33@gmail.com>
2023-09-17 14:37:47 +0100

    change function name and its parameter

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,6 +11,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	balance = binary_tree_heigh(tree->left) - binary_tree_heigh(tree->left);
+	balance = binary_tree_height(tree->left) - binary_tree_height(tree->right);
 	return (balance);
 }
