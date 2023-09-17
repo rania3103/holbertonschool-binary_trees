fc881acbd8c1ebe602cfbb43e3c4b66e90c7f030
rania3103 <raniaabassi33@gmail.com>
2023-09-17 16:03:47 +0100

    fix betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,16 +6,16 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    binary_tree_t *grandpa = (node->parent)->parent;
+	binary_tree_t *grandpa = (node->parent)->parent;
 
-    if (node == NULL)
-        return (NULL);
+	if (node == NULL)
+		return (NULL);
 
-    if (node->parent == NULL || grandpa == NULL)
-        return (NULL);
+	if (node->parent == NULL || grandpa == NULL)
+		return (NULL);
 
-    if (grandpa->right == node->parent)
-        return (grandpa->left);
-    else
-        return (grandpa->right);
+	if (grandpa->right == node->parent)
+		return (grandpa->left);
+	else
+		return (grandpa->right);
 }
