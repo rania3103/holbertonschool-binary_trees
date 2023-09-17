b30f7ad2f748710255ddc818cd1db282f68a6723
Mariam Charrada <charradamariam@yahoo.com>
2023-09-17 02:03:25 -0700

    remove tmp

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,7 +8,6 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
-	binary_tree_t *tmp;
 
 	if (parent == NULL || newnode == NULL)
 		return (NULL);
@@ -19,9 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = newnode;
 	else
 	{
-		tmp = parent->left;
-		tmp->parent = newnode;
-		newnode->left = tmp;
+		newnode->left = parent->left;
 		parent->left = newnode;
 	}
 
