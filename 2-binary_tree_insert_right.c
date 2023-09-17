f15ee43e3ea11e19a90bbaef366aee3f7316ba5e
rania3103 <raniaabassi33@gmail.com>
2023-09-17 12:27:33 +0100

    fix code

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -27,5 +27,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right = new_node;
 		new_node->right = tmp;
 	}
+	else
+		parent->right = new_node;
+
 	return (new_node);
 }
