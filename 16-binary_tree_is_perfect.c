05770cfdcf02fb0ff07eb28d6f00d45da6310755
amaalyy <amal.elzowawi@gmail.com>
2023-09-18 00:39:55 +0100

    fix betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,7 +27,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 /**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree: ptr
- * return: height
+ * Return: height
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
