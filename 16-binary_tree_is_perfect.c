a3512baca4431dcaffd525101e3d93c2b5068097
amaalyy <amal.elzowawi@gmail.com>
2023-09-17 22:24:37 +0100

    check is it perfect or not

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,26 +6,31 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height = 0 
-	left_leaves = 0 
-	right_height = 0
-	right_leaves = 0
+	size_t right_height, left_height;
 
-	if (!tree)
+	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height != right_height)
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (binary_tree_height(tree->right) == binary_tree_height(tree->left))
+	{
+		right_height = binary_tree_is_perfect(tree->right);
+		left_height = binary_tree_is_perfect(tree->left);
+	}
+	else
 	{
 		return (0);
 	}
-	left_leaves = binary_tree_leaves(tree->left);
-	right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves == right_leaves);
-
+	if (right_height == 1 && left_height == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
